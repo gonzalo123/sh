6c5ae2b4fe8d721d515861286f01978e927d770c
Gonzalo Ayuso <gonzalo123@gmail.com>
2012-11-16 00:12:40 +0100

    variable name error

--- lib/Sh/Parser.php
+++ lib/Sh/Parser.php
@@ -5,21 +5,21 @@ use Symfony\Component\Process\Process;
 
 class Parser
 {
-    public function getCommandToProcess($name, $comandArgument = NULL)
+    public function getCommandToProcess($name, $commandArgument = NULL)
     {
-        if (is_null($comandArgument)) return $name;
+        if (is_null($commandArgument)) return $name;
 
-        if (is_array($comandArgument)) {
-            $comandArgument = implode(' ', $this->surroundItemsWithSpacesWithQuotes($comandArgument));
+        if (is_array($commandArgument)) {
+            $commandArgument = implode(' ', $this->surroundItemsWithSpacesWithQuotes($commandArgument));
         }
 
-        return $name . ' ' . $comandArgument;
+        return $name . ' ' . $commandArgument;
     }
 
-    private function surroundItemsWithSpacesWithQuotes($comandArgument)
+    private function surroundItemsWithSpacesWithQuotes($commandArgument)
     {
         return array_map(function ($item) {
             return strpos($item, ' ') !== FALSE ? "'{$item}'" : $item;
-        }, $comandArgument);
+        }, $commandArgument);
     }
 }
